b77584f142e35107c2e58ad3f127973a654e7c7d
Lewis Clayton <mail@l.ew.is>
2012-05-06 18:20:07 +0100

    before filter on click_save

--- app/controllers/bounces_controller.rb
+++ app/controllers/bounces_controller.rb
@@ -1,5 +1,6 @@
 class BouncesController < ApplicationController
-    layout 'bounces'
+  after_filter :save_click
+  layout 'bounces'
   def index
     redirect
     render('redirect')
@@ -9,7 +10,16 @@ class BouncesController < ApplicationController
     uuid                    = UUID.new
     @click_uuid             = uuid.generate
     @link                   = Link.find(params[:id])
-    Click.create(:link_id => params[:id], :click_uuid => @click_uuid, :ref_url => request.env["HTTP_REFERER"], :ref_user_agent => request.env["HTTP_USER_AGENT"], :ref_ip => request.env["REMOTE_ADDR"])
+    @ref_url                = request.env["HTTP_REFERER"]
+    @ref_ip                 = request.env["REMOTE_ADDR"]
+    @ref_user_agent         = request.env["HTTP_USER_AGENT"]
+  end
+  
+  private
+  
+  def save_click
+    Click.create(:link_id => params[:id], :click_uuid => @click_uuid, :ref_url => @ref_url, :ref_user_agent => @ref_user_agent, :ref_ip => @ref_ip)
+    
   end
 end
 
